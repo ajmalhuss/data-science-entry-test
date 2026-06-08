578c171e61acceb81b6c0099faafad9a1f138f53
ajmalhuss <ajmal_h@yahoo.com>
2026-06-08 17:51:44 +0800

    Update q7.py

--- q7.py
+++ q7.py
@@ -5,13 +5,13 @@ class Car:
     - Initialize these attributes in the __init__ method
     - Add a method named describe_car() that prints information about the car as "Year Make Model"
     """
-    def __init__(self, make, model, year): #to automatically initialize the variables
-        self.make = make
-        self.model = model
-        self.year = year
+    def __init__(self, make, model, year): #to automatically assign the variables
+        self.make = make #assigns make to the given value mak
+        self.model = model #assigns model to the given value model
+        self.year = year #assigns year to the given value year
 
     def describe_car(self): #method to print the car
-        print(f"{self.make} {self.year} {self.model}")
+        print(f"{self.make} {self.year} {self.model}") #prints using the value assigned above
 
 
 # Task 2
